3abfdb788c349b426137005918b01fb8aa84931d
Clemens Schmid <clemisch@tuta.io>
2026-05-07 11:00:41 +0200

    Merge-squash branch phase-displace into dev
    Add 2ThetaPhaseFlatDetDispRatio to PowderPatternDiffraction
    
    ...for per-phase sample displacement
    
    ---
    
    Squashed commit of the following:
    
    commit a987df25f3db2af05d8b946d7bdf0b65db5eea07
    Author: Clemens Schmid <clemisch@tuta.io>
    Date:   Wed Apr 29 17:30:44 2026 +0200
    
        adapt test for sample+phase displacement
    
    commit 9997dc678f97a4a158f137e40037f8d195b0db55
    Author: Clemens Schmid <clemisch@tuta.io>
    Date:   Tue Apr 28 16:34:21 2026 +0200
    
        add 2ThetaFlatDetDispRatioPhase displacement per phase

--- tests/test_powderpattern.py
+++ tests/test_powderpattern.py
@@ -310,7 +310,37 @@ class TestPowderPatternBackground(unittest.TestCase):
 
 
 class TestPowderPatternDiffraction(unittest.TestCase):
-    pass
+    @pytest.fixture(autouse=True)
+    def prepare_fixture(self, loadcifdata):
+        self.loadcifdata = loadcifdata
+
+    def test_twotheta_flat_det_disp_ratio_phase(self):
+        c = self.loadcifdata("paracetamol.cif")
+        p = PowderPattern()
+        p.SetWavelength(0.7)
+        x = np.linspace(0, 40, 4001)
+        p.SetPowderPatternX(np.deg2rad(x))
+        p.SetPowderPatternObs(np.ones_like(x))
+        pd = p.AddPowderPatternDiffraction(c)
+        pd.SetReflectionProfilePar(
+            ReflectionProfileType.PROFILE_PSEUDO_VOIGT, 1e-6
+        )
+
+        self.assertEqual(pd.twotheta_flat_det_disp_ratio_phase, 0)
+
+        calc0 = np.array(p.GetPowderPatternCalc(), copy=True)
+        p.twotheta_flat_det_disp_ratio = 2e-3
+        pd.twotheta_flat_det_disp_ratio_phase = 1e-3
+        self.assertAlmostEqual(pd.twotheta_flat_det_disp_ratio_phase, 1e-3)
+
+        calc1 = np.array(p.GetPowderPatternCalc(), copy=True)
+        self.assertFalse(np.array_equal(calc0, calc1))
+
+        pd.twotheta_flat_det_disp_ratio_phase = 0
+        p.twotheta_flat_det_disp_ratio = 3e-3
+        calc2 = np.array(p.GetPowderPatternCalc(), copy=True)
+        self.assertTrue(np.allclose(calc1, calc2))
+
     # def test___init__(self):  assert False
     # def test_ExtractLeBail(self):  assert False
     # def test_GetExtractionMode(self):  assert False
